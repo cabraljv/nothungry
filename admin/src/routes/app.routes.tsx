8526d59cd03fe1faf5a5294c47112dac5bdf7c6c
cabraljv <joaovictorcabral.dev@gmail.com>
2020-07-18 05:46:51 -0300

    feat: add sidebar to routes

--- admin/src/routes/app.routes.tsx
+++ admin/src/routes/app.routes.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Sidebar from '../components/Sidebar';
+import {Container} from './styles'
 
 export default function AppRoutes() {
   return (
     <>
-        <Route path="/" exact component={Dashboard} />
+      <Sidebar />
+        <Container>
+          <Route path="/" exact component={Dashboard} />
+      </Container>
     </>
   );
 }
