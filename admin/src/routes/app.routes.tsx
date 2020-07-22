5e1c94aab938d52e2408ef61958bdfe269b27f42
cabraljv <joaovictorcabral.dev@gmail.com>
2020-07-22 04:24:06 -0300

    feat: add socket io to admin page

--- admin/src/routes/app.routes.tsx
+++ admin/src/routes/app.routes.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route} from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Sidebar from '../components/Sidebar';
+import OrderRequest from '../components/OrderRequest'
 import {Container} from './styles'
+import io from 'socket.io-client'
 import {ToastContainer} from 'react-toastify';
-
+import {useAuth} from '../hooks/auth'
+interface Product{ 
+  id: string;
+  name: string;
+}
+interface IOrder{
+  products: Product[];
+  reciver: string;
+  adress: string;
+  observation: string;
+  reference: string;
+  payment_method: number;
+  whatsapp: string;
+}
 export default function AppRoutes() {
+  const {token} = useAuth();
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
+  const [newOrder, setNewOrder] = useState<IOrder>();
+
+  useEffect(()=>{
+     
+    setSocket(io(process.env.REACT_APP_API_URL || '',{ query:{
+      token: `Bearer ${token}`
+    }}))
+  },[token])
+  useEffect(()=>{
+    socket?.on('newOrder',(order: IOrder)=>{
+      setNewOrder(order);
+    })
+  },[socket]);
+
   return (
     <>
       <Sidebar />
+      {
+        newOrder && <OrderRequest order={newOrder} />
+      }
       <Container>
           <Route path="/" exact component={Dashboard} />
       </Container>
