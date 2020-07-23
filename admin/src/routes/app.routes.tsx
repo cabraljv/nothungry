9233c3ca9061e9fe74dddfef304ffa48e6a2c576
cabraljv <joaovictorcabral.dev@gmail.com>
2020-07-23 01:25:42 -0300

    feat: remove obsoleted component from app routes

--- admin/src/routes/app.routes.tsx
+++ admin/src/routes/app.routes.tsx
@@ -1,13 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Route} from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Sidebar from '../components/Sidebar';
-import OrderRequest from '../components/OrderRequest'
 import {Container} from './styles'
-import io from 'socket.io-client'
 import {ToastContainer} from 'react-toastify';
-import {useAuth} from '../hooks/auth'
 interface Product{ 
   id: string;
   name: string;
@@ -23,28 +20,10 @@ interface IOrder{
   whatsapp: string;
 }
 export default function AppRoutes() {
-  const {token} = useAuth();
-  const [socket, setSocket] = useState<SocketIOClient.Socket>();
-  const [newOrder, setNewOrder] = useState<IOrder | null>(null);
-
-  useEffect(()=>{
-     
-    setSocket(io(process.env.REACT_APP_API_URL || '',{ query:{
-      token: `Bearer ${token}`
-    }}))
-  },[token])
-  useEffect(()=>{
-    socket?.on('newOrder',(order: IOrder)=>{
-      setNewOrder(order);
-    })
-  },[socket]);
-
   return (
     <>
       <Sidebar />
-      {
-        newOrder && <OrderRequest order={newOrder} handleClose={()=>setNewOrder(null)}/>
-      }
+
       <Container>
           <Route path="/" exact component={Dashboard} />
       </Container>
