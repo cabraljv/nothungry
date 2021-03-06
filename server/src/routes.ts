e0ca4374b9b16b1493a068681703b44a5276765c
cabraljv <joaovictorcabral.dev@gmail.com>
2021-03-06 02:36:42 -0300

    update: update libs

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,15 +2,15 @@ import { Router } from 'express';
 import Multer from 'multer';
 
 import uploadConfig from './config/upload';
+import OrderAcceptController from './controllers/OrderAcceptController';
 import OrderController from './controllers/OrderController';
+import OrderDenyController from './controllers/OrderDenyController';
+import OrderFinishController from './controllers/OrderFinishController';
 import ProductController from './controllers/ProductController';
 import RestaurantController from './controllers/RestaurantController';
 import SessionController from './controllers/SessionController';
 import WhatsappMessageController from './controllers/WhatsappMessageController';
 import auth from './middlewares/auth';
-import OrderAcceptController from './controllers/OrderAcceptController';
-import OrderDenyController from './controllers/OrderDenyController';
-import OrderFinishController from './controllers/OrderFinishController';
 
 const uploads = Multer(uploadConfig);
 const routes = Router();
@@ -30,7 +30,7 @@ routes.put('/order/accept/:orderId', OrderAcceptController.store);
 routes.put('/order/deny/:orderId', OrderDenyController.store);
 routes.put('/order/finish/:orderId', OrderFinishController.store);
 
-routes.get('/order', OrderController.index)
+routes.get('/order', OrderController.index);
 
 routes.post('/product', uploads.single('image'), ProductController.store);
 export default routes;
