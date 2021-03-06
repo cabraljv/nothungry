e2bf9bb9800e02bdf5e8ccdf63d11bba6f2f65ae
cabraljv <joaovictorcabral.dev@gmail.com>
2021-03-06 02:35:50 -0300

    feat: modify user jorney

--- server/src/controllers/OrderAcceptController.ts
+++ server/src/controllers/OrderAcceptController.ts
@@ -1,26 +1,27 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
 import Order from '../models/Order';
-import {getRepository} from 'typeorm';
-import Restaurant from '../models/Restaurant';
 
-class OrderAcceptController{
-  async store(req: Request,res: Response){
-    const {orderId} = req.params;
+class OrderAcceptController {
+  async store(req: Request, res: Response) {
+    const { orderId } = req.params;
     const orderRepo = getRepository(Order);
-    
-    const order = await orderRepo.findOne(orderId, {relations:['restaurant']});
-    if(!order){
-      return res.status(404).json({error: 'Order does not exists'});
-    } 
-    
-  
-    if(order.restaurant.id!==req.userId){
-      return res.status(401).json({error: 'Unauthorized'});
-    }
-    order.accepted=true;
-    orderRepo.save(order);
-    return res.json({response: 'Order successfull accepted'});
 
+    const order = await orderRepo.findOne(orderId, {
+      relations: ['restaurant'],
+    });
+    if (!order) {
+      return res.status(404).json({ error: 'Order does not exists' });
+    }
+    if (typeof order.restaurant !== 'string') {
+      if (order.restaurant.id !== req.userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    }
+    order.accepted = true;
+    orderRepo.save(order);
+    return res.json({ response: 'Order successfull accepted' });
   }
 }
 export default new OrderAcceptController();
--- server/src/controllers/OrderController.ts
+++ server/src/controllers/OrderController.ts
@@ -5,31 +5,65 @@ import * as Yup from 'yup';
 import Order from '../models/Order';
 
 class OrderController {
-  async index(req: Request, res: Response){
+  async index(req: Request, res: Response) {
     const orderRepo = getRepository(Order);
     const orders = await orderRepo
       .createQueryBuilder('order')
-      .where({ restaurant: req.userId, concluided: false, accepted: true, denied: false })
+      .where({
+        restaurant: req.userId,
+        concluided: false,
+        accepted: true,
+        denied: false,
+      })
       .leftJoinAndSelect('order.products', 'products')
-      .select(['order.adress','order.reference','products.name', 'products.id', 'order.id','order.observation','order.whatsapp','order.created_at','order.payment_method', 'order.accepted', 'order.reciver'])
-      .getMany()
-      const pendingOrders = await orderRepo
+      .select([
+        'order.adress',
+        'order.reference',
+        'products.name',
+        'products.id',
+        'order.id',
+        'order.observation',
+        'order.whatsapp',
+        'order.created_at',
+        'order.payment_method',
+        'order.accepted',
+        'order.reciver',
+      ])
+      .getMany();
+    const pendingOrders = await orderRepo
       .createQueryBuilder('order')
-      .where({ restaurant: req.userId, concluided: false, accepted: false, denied: false })
+      .where({
+        restaurant: req.userId,
+        concluided: false,
+        accepted: false,
+        denied: false,
+      })
       .leftJoinAndSelect('order.products', 'products')
-      .select(['order.adress','order.reference','products.name', 'products.id', 'order.id','order.observation','order.whatsapp','order.created_at','order.payment_method', 'order.accepted', 'order.reciver'])
-      .getMany()
-    return res.json({orders, pendingOrders})
+      .select([
+        'order.adress',
+        'order.reference',
+        'products.name',
+        'products.id',
+        'order.id',
+        'order.observation',
+        'order.whatsapp',
+        'order.created_at',
+        'order.payment_method',
+        'order.accepted',
+        'order.reciver',
+      ])
+      .getMany();
+    return res.json({ orders, pendingOrders });
   }
+
   async store(req: Request, res: Response) {
     const schema = Yup.object().shape({
+      id: Yup.string().required(),
       reciver: Yup.string().required(),
       adress: Yup.string().required(),
       observation: Yup.string().required(),
       reference: Yup.string().required(),
-      whatsapp: Yup.string().required(),
       payment_method: Yup.number().required(),
-      restaurant_id: Yup.string().required(),
       products: Yup.array().required(),
     });
     if (!(await schema.validate(req.body)))
@@ -37,12 +71,11 @@ class OrderController {
 
     const orderRepo = getRepository(Order);
     const {
+      id,
       reciver,
       adress,
-      whatsapp,
       observation,
       reference,
-      restaurant_id,
       payment_method,
       products,
     } = req.body;
@@ -50,22 +83,40 @@ class OrderController {
     for (let i = 0; i < products.length; i += 1) {
       total += products[i].price;
     }
-    const order = orderRepo.create({
-      reciver,
-      adress,
-      reference,
-      whatsapp,
-      observation,
-      total,
-      restaurant: restaurant_id,
-      payment_method,
-      products,
-    });
-    await orderRepo.save(order);
-    const restaurantSocketId  = req.connectedClients[restaurant_id];
-    req.io.to(restaurantSocketId).emit('newOrder', order);
+    console.log(id);
 
-    return res.json({ response: 'Order successfull created' });
+    try {
+      const order = await orderRepo.findOne({
+        where: { id },
+        relations: ['restaurant'],
+      });
+
+      if (!order) {
+        return res.status(404).json({ error: 'This order not exists' });
+      }
+      if (order.sended) {
+        return res.status(401).json({ error: 'This order cant be changed' });
+      }
+
+      order.adress = adress;
+      order.payment_method = payment_method;
+      order.reciver = reciver;
+      order.observation = observation;
+      order.products = products;
+      order.reference = reference;
+      order.total = total;
+
+      orderRepo.save(order);
+      console.log(order);
+      await orderRepo.save(order);
+      if (typeof order.restaurant !== 'string') {
+        const restaurantSocketId = req.connectedClients[order.restaurant.id];
+        req.io.to(restaurantSocketId).emit('newOrder', order);
+      }
+      return res.json({ response: 'Order successfull created' });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   }
 }
 
